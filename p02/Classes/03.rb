2f9957e6b9bfae92a86e93cfb2681509d424b3f2
Roberto Albanesi <beto0607@gmail.com>
2018-09-14 22:41:53 -0300

    initiated exercice 5

--- p02/Classes/03.rb
+++ p02/Classes/03.rb
@@ -1,1 +1,17 @@
+module Reverso
+  def di_tcejbo
+    self.object_id.to_s.reverse!
+  end
+  def ssalc
+    self.class.to_s.reverse!
+  end
+end
 
+class TestReverso
+  include Reverso
+end
+
+t = TestReverso.new
+
+puts t.di_tcejbo
+puts t.ssalc
--- p02/Classes/04.rb
+++ p02/Classes/04.rb
@@ -1,1 +1,57 @@
+module Countable
 
+  def Countable.included(mod)
+    @last_method = nil
+  end
+
+  def counters
+    @counters ||= {}
+  end
+
+  def count_invocations_of(sym)
+    counters[sym] = 0
+    sym_old = (sym.to_s + "_old").to_sym
+
+    Countable::ºdefine_method(sym_old) do |*arguments|
+      counters[sym] += 1
+      puts "define_method"
+      self.send(sym, *arguments)
+    end
+    Countable::alias_method  sym, sym_old
+
+  end
+
+  def invoked?(sym)
+    puts counters[sym] > 0
+  end
+
+  def invoked(sym)
+    puts counters[sym]
+  end
+
+end
+
+
+class Greeter
+  include Countable
+
+  def initialize
+    count_invocations_of :hi
+  end
+
+  def hi
+    puts 'hi!'
+  end
+
+  def bye
+    puts 'bye!'
+  end
+end
+
+a = Greeter.new
+
+a.invoked? :hi
+a.hi
+a.invoked? :hi
+
+p a.counters
