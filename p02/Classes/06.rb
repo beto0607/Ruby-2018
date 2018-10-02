f246934b5cdb865189c02dc86dfaa02eaab4a3a1
Roberto Albanesi <beto0607@gmail.com>
2018-10-01 21:28:09 -0300

    done all exercices of classes

--- p02/Classes/06.rb
+++ p02/Classes/06.rb
@@ -1,1 +1,22 @@
+module GenericFactory #se modifico clas a module
 
+  def create(**args)#se borro self.
+    new(**args)
+  end
+  def initialize(**args)
+    raise NotImplementedError
+  end
+end
+
+class FirstChild # se borro < GenericFactory
+  extend GenericFactory #se agrego el include
+  attr_reader :var_1
+  def initialize(**args)
+    @var_1 = args[:var_1]
+  end
+
+end
+
+a = FirstChild.create({:var_1=>"af"})
+p a
+puts a.var_1
